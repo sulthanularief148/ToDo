c108b25a038779498966a891024abc0e1e67e845
Sulthanul Arief Mohamed <arief@xerago.com>
2022-11-23 14:11:13 +0530

    This is To do list First Part"

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 const uc = require("upper-case");
-const firstLetterCap = require("capitalize");
-const test = require('tape')
+// const firstLetterCap = require("capitalize");
+// const test = require('tape')
 
 //const localStorage = require("node-localstorage")
 let items = ["Biriyani", "Fish"];
-let workItems = ["singapore"];
+let workItem = ["singapore"];
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -23,25 +24,27 @@ res.render("list", {listTitle:today,newListItems: items });
 
 
 app.post("/", function(req, res) {
-  let item = req.body.addList;
-  let capital = item.slice(0,1).toUpperCase() + item.slice(1,item.length).toLowerCase();
+  let item = req.body.addList.slice(0,1).toUpperCase() + req.body.addList.slice(1,req.body.addList.length).toLowerCase(); ;
+  
   
   
   //const ucItem = firstLetterCap.Capitalize(req.body.addList);
  // console.log(ucItem);
-
-  if(req.body.list === "Work") {
-    workItems.push(capital);
+ 
+  if(req.body.list === "work Items") {
+    workItem.push(item);
     res.redirect("/work");
   }
    else {
-    items.push(capital);
+    items.push(item);
     res.redirect("/");
    }
+
+
 });
 
 app.get("/work", function(req,res) {
-  res.render("list", {listTitle: "work Items", newListItems: workItems});
+  res.render("list", {listTitle: "work Items", newListItems: workItem});
 });
 
 app.get("/about", function(req,res) {
